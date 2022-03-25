e393af3e4516976442740986e94320466f5a4691
kiichiro <5821157@gmail.com>
2022-03-25 17:04:03 +0900

    数値の修正

--- circleci_dir/tests/match-test.py
+++ circleci_dir/tests/match-test.py
@@ -13,7 +13,7 @@ from src.match import addition
 class MathTest(unittest.TestCase):
     def test_addition(self):
         actual = addition(3,4)
-        expected = 7 # 実行結果と一致
+        expected = 8 # 実行結果と一致
         self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
